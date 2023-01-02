3b203b41ad9b937a318d2ecc5312661d5928fa61
AldiAgeng <aldiageng48@gmail.com>
2023-01-02 14:23:23 +0700

    delete expireIn

--- app/utils/authUtil.js
+++ app/utils/authUtil.js
@@ -29,7 +29,7 @@ module.exports = {
     },
 
     createToken(payload) {
-        return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "24h" })
+        return jwt.sign(payload, process.env.SECRET_KEY)
     },
 
     decodedToken(token) {
